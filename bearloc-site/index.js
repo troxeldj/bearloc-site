dce9fbb0813fbf68f967ff9cf11087488df2917b
Dillon Troxell <91560261+troxeldj@users.noreply.github.com>
2023-07-12 20:34:02 +0000

    first commit

---
+++ bearloc-site/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+let ejs = require('ejs');
+const PORT = 3000;
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {});
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
