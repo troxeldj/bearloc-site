3e72a47ee93b97ad09afecb31fc17939ccd03ffe
Dillon Troxell <91560261+troxeldj@users.noreply.github.com>
2023-07-13 03:12:27 +0000

    Added a bit to homescreen. Added Static Folder.

--- bearloc-site/index.js
+++ bearloc-site/index.js
@@ -6,9 +6,11 @@ const PORT = 3000;
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.render('index', {});
-});
+})
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
