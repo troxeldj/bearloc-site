ee5417332a958c1364c794718243f70b7c01e5fa
Dillon Troxell <troxeldj@mail.uc.edu>
2023-07-14 16:24:57 -0400

    Added Search Page and route. Parsed Body for number of beds and bath.

--- bearloc-site/index.js
+++ bearloc-site/index.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 let ejs = require('ejs');
 const PORT = 3000;
-
+let bodyParser = require('body-parser');
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('index', {});
 })
+
+app.post('/search', (req,res) => {
+    res.send(`Num Bed: ${req.body.bedrooms}` + "<br>" +  `Num Bath: ${req.body.bathrooms}`)
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
